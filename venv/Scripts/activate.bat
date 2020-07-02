2f5a6815678b60daba4820ed3d1dd19ff00307a4
BernhardKoschicek <b.koschicek@gmail.com>
2020-07-02 16:17:58 +0200

    debug

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\bkoschicek\Desktop\web\orthodoxes-europa\venv"
+set "VIRTUAL_ENV=C:\Users\bkoschicek\Desktop\web\orthodoxes_europa\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
