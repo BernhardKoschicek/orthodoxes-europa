a7139034523d38509fe21ed1c61fde19e37b9b0d
BernhardKoschicek <b.koschicek@gmail.com>
2020-07-22 17:23:53 +0200

    front page

--- orthodoxes_europa/views/index.py
+++ orthodoxes_europa/views/index.py
@@ -5,7 +5,6 @@ from flask import render_template
 from orthodoxes_europa import app
 
 
-
 @app.route('/')
 def home() -> str:
     front_menu = OrderedDict([
@@ -35,7 +34,23 @@ def home() -> str:
         })
     ])
 
-    return render_template('home.html', front_menu=front_menu)
+    home_gallery = {
+        'lageplan.jpg': 'Lageplan des Flüchtlingslagers Oberhollabrunn '
+                        '(<a href="http://www.ffhollabrunn.at/index.php?option=com_content&view=article&id=1159:kommandantentafel&catid=98:geschichte&Itemid=71" target="_blank">http://www.ffhollabrunn.at/</a>)',
+        'lager_oberhollabrunn.jpg': 'Das Eingangstor zum k.k. Flüchtlingslager Oberhollabrunn im Jahre'
+                                    ' 1916 Ecke Wienerstraße/Dr. Kutschergasse, 2020 Hollabrunn'
+                                    ' (<a href="https://regiowiki.at/wiki/Datei:Stockerau_Fl%C3%BCchtlingslager.jpg" target="_blank">Wikipedia</a>)',
+        'totenschaubefund.jpg': 'Beispiel eines Totenbeschaubefundes (Metropolis von Austria,'
+                                ' Archiv der griech.-orient. Kirchengemeinde zur Hl. Dreifaltigkeit, 1010 Wien)',
+        'sveti_sava.jpg': 'Die serbisch-orthodoxe Kirche zum Heiligen Sava in 1030 Wien (Mihailo St. Popović)',
+        'unterschrift_velimirovic.jpg': 'Ölgemälde des ersten serbischen Erzbischofs des Hl. Sava von Uroš Predić (1857-1953) (Mihailo St. Popović)',
+        'uros_predic.jpg': 'Unterschrift des Hl. Bischofs Nikolaj Velimirović (1881-1956) im Evangelium der Kirche des Hl. Sava (Mihailo St. Popović)'
+    }
+
+    return render_template('home.html', front_menu=front_menu, home_gallery=home_gallery)
+
+
+
 
 
 @app.errorhandler(404)
