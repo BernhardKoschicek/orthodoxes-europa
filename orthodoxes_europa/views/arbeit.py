38672e2796d150bfb0f0d04db513ad084cfdd7d3
BernhardKoschicek <b.koschicek@gmail.com>
2021-01-18 23:46:47 +0100

    öffentlichkeitsarbeit überarbeitet

--- orthodoxes_europa/views/arbeit.py
+++ orthodoxes_europa/views/arbeit.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 
-from flask import render_template, url_for
+from flask import render_template
 
 from orthodoxes_europa import app
 
@@ -8,26 +8,59 @@ from orthodoxes_europa import app
 @app.route('/öffentlichkeitsarbeit')
 def öffentlichkeitsarbeit():
     public = OrderedDict([
+        ('Weihnachten', {
+            'id': 'Weihnachten',
+            'type': 'Video',
+            'date': '07. Jänner 2021',
+            'title': 'Warum feiern die orthodoxen Kirchen am 7. Jänner Weihnachten?',
+            'image': 'weihnachten.jpg',
+            'description': """Zum Jahreswechsel hat Mihailo Popović für die Salesianische
+             Jugendbewegung Österreich ein Interview gegeben. In der Sendung "Frag den Don" von P.
+              Johannes Haas von den Salesianern Don Boscos hat er über das Thema "Warum feiern
+               die orthodoxen Kirchen am 7. Jänner Weihnachten?" gesprochen...
+                """,
+            'text': """Zum Jahreswechsel hat <b>Mihailo Popović</b> für die <b><a href='https://www.donbosco4youth.at/' target='_blank'>Salesianische Jugendbewegung
+             Österreich</a></b> ein Interview gegeben. In der Sendung "Frag den Don" von P. Johannes Haas
+              von den Salesianern Don Boscos hat er über das Thema <b>"Warum feiern die orthodoxen
+               Kirchen am 7. Jänner Weihnachten?"</b> gesprochen.<br>
+                Sehen Sie dazu bitte folgenden Link: 
+                <a href='https://www.youtube.com/watch?v=2McBHjhU_Kg' target='_blank'>https://www.youtube.com/watch?v=2McBHjhU_Kg</a>""",
+            'attachment': '<div class="pt-2">'
+                          '<iframe width="100%" height="500px" src="https://www.youtube.com/embed/2McBHjhU_Kg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>'
+                          '</iframe></div>'
+        }),
         ('Kathpress', {
+            'id': 'Spuren',
             'type': 'Zeitungsartikel',
             'date': '08. Dezember 2020',
             'title': 'Auf den Spuren orthodoxer Weltkriegsflüchtlinge in Hollabrunn',
+            'image': 'spuren.jpg',
             'description': """<p>Die kathpress (Katholische Presseagentur Österreich) berichtet in ihrer
-             Information Orthodoxie (Ausgabe Nr. 76, 8. Dezember 2020, Seite 7;
-              <a href="https://www.kathpress.at/goto/meldung/1964124/Auf_den_Spuren_orthodoxer_Weltkriegsfl__chtlinge_in_Hollabrunn"
-               target="_blank">https://www.kathpress.at/goto/meldung/1964124/Auf_den_Spuren_orthodoxer_Weltkriegsfl__chtlinge_in_Hollabrunn</a>)
+             Information Orthodoxie (Ausgabe Nr. 76, 8. Dezember 2020, Seite 7)
                 umfassend über unser Forschungsprojekt "Auf der Flucht in der Monarchie – das Schicksal
-                 der orthodoxen Flüchtlinge im Lager Oberhollabrunn (1914-1918)".
-                </p>
-                <p>Bitte sehen Sie dazu im Detail folgenden <a href="{url}" target="_blank">Artikel</a> 
-                </p>
-                """.format(url=url_for('static',
-                                       filename='repository/Kathpress_Orthodoxie_Flüchtlinge_Hollabrunn.pdf'))}),
+                 der orthodoxen Flüchtlinge im Lager Oberhollabrunn (1914-1918)"...
+                """,
+            'text': """<p>Die kathpress (Katholische Presseagentur Österreich) berichtet in ihrer
+         Information Orthodoxie (Ausgabe Nr. 76, 8. Dezember 2020, Seite 7;
+          <a href="https://www.kathpress.at/goto/meldung/1964124/Auf_den_Spuren_orthodoxer_Weltkriegsfl__chtlinge_in_Hollabrunn"
+           target="_blank" class="wrapword">https://www.kathpress.at/goto/meldung/1964124/Auf_den_Spuren_orthodoxer_Weltkriegsfl__chtlinge_in_Hollabrunn</a>)
+            umfassend über unser Forschungsprojekt "Auf der Flucht in der Monarchie – das Schicksal
+             der orthodoxen Flüchtlinge im Lager Oberhollabrunn (1914-1918)".
+             </p>""",
+            'attachment': '<embed src="static/repository/Kathpress_Orthodoxie_Flüchtlinge_Hollabrunn.pdf" width="100%" height="1100px" />'
+        }),
         ('Le Monde', {
-        'type': 'Zeitungsartikel',
-        'date': '05. Juni 2020',
-        'title': 'Vienne, impératrice de l’Est',
-        'description': """<p>Die bekannte französische Tageszeitung Le Monde hat einen Beitrag über
+            'id': 'Vienne',
+            'type': 'Zeitungsartikel',
+            'date': '05. Juni 2020',
+            'title': 'Vienne, impératrice de l’Est',
+            'image': 'balkan.jpg',
+            'description': """<p>Die bekannte französische Tageszeitung Le Monde hat einen Beitrag über
+            die Bedeutung der Stadt Wien für die Menschen in Ost- und Südosteuropa im Lauf der
+             Vergangenheit und Gegenwart im Druck und online veröffentlicht. Darin kommt unter
+              anderem Mihailo Popović zum digitalen Geoportal und der Geschichte der Wiener Serben zu
+               Wort...""",
+            'text': """<p>Die bekannte französische Tageszeitung Le Monde hat einen Beitrag über
         die Bedeutung der Stadt Wien für die Menschen in Ost- und Südosteuropa im Lauf der
          Vergangenheit und Gegenwart im Druck und online veröffentlicht. Darin kommt unter
           anderem Mihailo Popović zum digitalen Geoportal und der Geschichte der Wiener Serben zu
@@ -49,7 +82,7 @@ def öffentlichkeitsarbeit():
                      target="_blank">https://www.lemonde.fr/m-le-mag/article/2020/06/05/vienne-imperatrice-de-l-est_6041894_4500055.html</a></cite> )</footer>
                     </blockquote>
         """
-    })
-                          ])
+        })
+    ])
 
     return render_template('arbeit.html', public=public)
